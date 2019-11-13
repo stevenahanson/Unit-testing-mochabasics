68f299a717203b47ad376fe080181c15720c74f9
Steven Hanson <steven.hanson@wintechnologies.net>
2019-11-13 15:43:43 +0000

    Add two math tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,20 @@
 var assert = require('assert'); // Require the built in 'assertion' library
 
-describe('Array', function() { // test group 'Array'
+// describe('Array', function() { // test group 'Array'
 
-    describe('#indexOf()', function() { // nested test group '#indexOf()' 
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(4));
+//     describe('#indexOf()', () => { // nested test group '#indexOf()' 
+//     it('should return -1 when the value is not present', function(){
+//       assert.equal(-1, [1,2,3].indexOf(4));
+//     });
+//   });
+// });
+
+describe('Math', () => {
+    it('should test if 3*3 = 9', () => {
+        assert.equal(9, 3*3);
     });
-  });
-});
+
+    it('should test if (3-4)*8 = -8', () => {
+        assert.equal(-8, (3-4)*8);
+    })
+})
